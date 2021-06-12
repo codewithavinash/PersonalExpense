286909c818e3aee882593d23cf6b8fd19742cbc2
codewithavinash <avinashthakur.edu@gmail.com>
2021-06-12 18:36:57 +0530

    keys are added to the Transaction List

--- lib/widgets/transList.dart
+++ lib/widgets/transList.dart
@@ -10,75 +10,76 @@ class TransList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: transactions.isEmpty
-          ? LayoutBuilder(builder: (ctx, constraints) {
-              return Column(
-                children: [
-                  Text(
-                    'No Transactions added yet!',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    height: constraints.maxHeight * 0.7,
-                    child: Image.asset(
-                      'assets/fonts/images/rocket.png',
-                      fit: BoxFit.cover,
+        child: transactions.isEmpty
+            ? LayoutBuilder(builder: (ctx, constraints) {
+                return Column(
+                  children: [
+                    Text(
+                      'No Transactions added yet!',
+                      style: Theme.of(context).textTheme.headline6,
                     ),
-                  ),
-                ],
-              );
-            })
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                //   return Card(
-                //     child: Row(
-                //       children: <Widget>[
-                //         Container(
-                //           margin:
-                //               EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                //           decoration: BoxDecoration(
-                //             border: Border.all(
-                //                 color: Theme.of(context).primaryColorDark,
-                //                 width: 2),
-                //           ),
-                //           padding: EdgeInsets.all(10),
-                //           child: Text(
-                //             '\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                //             style: Theme.of(context).textTheme.headline6,
-                //           ),
-                //         ),
-                //         Column(
-                //           mainAxisAlignment: MainAxisAlignment.start,
-                //           crossAxisAlignment: CrossAxisAlignment.start,
-                //           children: [
-                //             Text(
-                //               transactions[index].title,
-                //               style: Theme.of(context).textTheme.headline6,
-                //             ),
-                //             Text(
-                //               DateFormat.yMMMd().format(transactions[index].date),
-                //               style: TextStyle(
-                //                 fontSize: 14,
-                //                 color: Colors.grey,
-                //               ),
-                //             ),
-                //           ],
-                //         )
-                //       ],
-                //     ),
-                //   );
-                return TransactionItem(
-                  transaction: transactions[index],
-                  deleteTx: deleteTx,
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: constraints.maxHeight * 0.7,
+                      child: Image.asset(
+                        'assets/fonts/images/rocket.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
                 );
-              },
-              itemCount: transactions.length,
-              // children: transactions.map((tx) {}
-            ),
-      //.toList()
-    );
+              })
+            : ListView(
+                children:
+                    //   return Card(
+                    //     child: Row(
+                    //       children: <Widget>[
+                    //         Container(
+                    //           margin:
+                    //               EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    //           decoration: BoxDecoration(
+                    //             border: Border.all(
+                    //                 color: Theme.of(context).primaryColorDark,
+                    //                 width: 2),
+                    //           ),
+                    //           padding: EdgeInsets.all(10),
+                    //           child: Text(
+                    //             '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                    //             style: Theme.of(context).textTheme.headline6,
+                    //           ),
+                    //         ),
+                    //         Column(
+                    //           mainAxisAlignment: MainAxisAlignment.start,
+                    //           crossAxisAlignment: CrossAxisAlignment.start,
+                    //           children: [
+                    //             Text(
+                    //               transactions[index].title,
+                    //               style: Theme.of(context).textTheme.headline6,
+                    //             ),
+                    //             Text(
+                    //               DateFormat.yMMMd().format(transactions[index].date),
+                    //               style: TextStyle(
+                    //                 fontSize: 14,
+                    //                 color: Colors.grey,
+                    //               ),
+                    //             ),
+                    //           ],
+                    //         )
+                    //       ],
+                    //     ),
+                    //   );
+                    transactions
+                        .map((tx) => TransactionItem(
+                              key: ValueKey(tx.id),
+                              transaction: tx,
+                              deleteTx: deleteTx,
+                            ))
+                        .toList(),
+              )
+
+        //.toList()
+        );
   }
 }
